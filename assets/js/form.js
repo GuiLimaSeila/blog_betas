d8e3ea57fb4c6551c2e92f7e0968404f8be7b62b
juujminhau <juliaferreiradasilva.564@gmail.com>
2023-05-18 16:35:16 -0300

    oi

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,9 @@
-let nome = document.getElementById("nomeput").innerHTML
-let data = document.getElementById("dataput").innerHTML
-let descri = document.getElementById("descriput").innerHTML
+var nome = document.getElementById("nomeput").innerHTML;
+var data = document.getElementById("dataput").innerHTML;
+var descri = document.getElementById("descriput").innerHTML;
+var infos = [];
+
+function addComent(descri, data, nome){
+    
+    infos = nome, data, descri;
+}
