f5edd08ff9e21f422fe779c5e9077f919f8faf14
juujminhau <juliaferreiradasilva.564@gmail.com>
2023-05-23 14:56:47 -0300

    oi

--- assets/js/form.js
+++ assets/js/form.js
@@ -9,21 +9,27 @@ var msg = "";
 
 
 
+function addComent(titulo, nome, data, comen) {
+    arrayGeral.pop();
+    arrayGeral.pop();
+    arrayGeral.pop();
+    arrayGeral.pop();
 
-function addComent() {
     do {
     /*0*/ titulo = document.getElementById("tituput").value;
     arrayGeral.push(titulo)
-    document.getElementById("tituput").value = ''
+    document.getElementById("tituput").value = '';
     /*1*/ nome = document.getElementById("nomeput").value;
     arrayGeral.push(nome)
-    document.getElementById("nomeput").value = ''
+    document.getElementById("nomeput").value = '';
     /*2*/ data = document.getElementById("dataput").value;
     arrayGeral.push(data)
-    document.getElementById("dataput").value = ''
+    document.getElementById("dataput").value = '';
     /*3*/ comen = document.getElementById("comenput").value;
     arrayGeral.push(comen)
-    document.getElementById("comenput").value = ''
+    document.getElementById("comenput").value = '';
+
+    //fazer um bloco de comentario
     msg += `<div id="comentario">
     <p id="titulo2">Titulo: ${arrayGeral[0]} </p>  </p>
     <p id="titulo2">Nome: ${arrayGeral[1]} </p> </p>
@@ -32,21 +38,27 @@ function addComent() {
     <button type="button" class="btn2" onclick="editar()">[e]</button>
     <button type="button" class="btn2"onclick="apagar()">[d]</button>
     </div>`
-
-    document.getElementById("comentarios").innerHTML = msg
+    document.getElementById("comentarios").innerHTML = msg;
     }
-    while (editar == false);
+    while (editar , apagar == false);
 }
 function editar(){
     document.getElementById("tituput").value = arrayGeral[0];
-    arrayGeral.pop();
     document.getElementById("nomeput").value = arrayGeral[1];
-    arrayGeral.pop();
     document.getElementById("dataput").value = arrayGeral[2];
-    arrayGeral.pop();
     document.getElementById("comenput").value = arrayGeral[3];
+    msg = ``;
+}
+function apagar(){
+    msg = "";
     arrayGeral.pop();
-    
+    arrayGeral.pop();
+    arrayGeral.pop();
+    arrayGeral.pop();
+    document.getElementById("tituput").value = '';
+    document.getElementById("nomeput").value = '';
+    document.getElementById("dataput").value = '';
+    document.getElementById("comenput").value = '';
 }
 
 
