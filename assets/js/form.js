e0d5911545fd6aa269a40f2e5516afcb7e24a451
juujminhau <juliaferreiradasilva.564@gmail.com>
2023-05-23 10:44:24 -0300

    oi

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,23 @@
-var nome = document.getElementById("nomeput").innerHTML;
-var data = document.getElementById("dataput").innerHTML;
-var comen = document.getElementById("comenput").innerHTML;
-var arrayGeral = [nome, data, comen];
 
+var arrayGeral = [];
+
+function addComent() {
+    var nome = document.getElementById("nomeput").value;
+    arrayGeral.push(nome)
+    document.getElementById("nomeput").value = ''
+    var data = document.getElementById("dataput").value;
+    arrayGeral.push(data)
+    document.getElementById("dataput").value = ''
+    var comen = document.getElementById("comenput").value;
+    arrayGeral.push(comen)
+    document.getElementById("comenput").value = ''
+    let msg = "";
+    msg += `<div id="comentario"><p> ${arrayGeral[0]} </p> <p> ${arrayGeral[1]} </p> <p> ${arrayGeral[2]} </p> </div>`;
+    arrayGeral.pop()
+    arrayGeral.pop()
+    arrayGeral.pop()
+    document.getElementById("comentarios").innerHTML = msg
+}
 
 
 
