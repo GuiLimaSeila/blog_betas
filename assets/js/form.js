5ee79552fdb2ffa263eefbd28b64baef47d50ebd
juujminhau <juliaferreiradasilva.564@gmail.com>
2023-06-15 08:39:58 -0300

    oi

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,54 +1,52 @@
-var arrayGeral = [];
-var arrayComents = [];
-var titulo;
 var nome;
 var data;
 var descri;
 var indice = -1;
 var msg = "";
 
-var tarefa = [];
+var arrayGeral = [];
 
 function create() {
   if (indice < 0) {
-    /*0*/ titulo = document.getElementById("tituput").value;
+    var titulo = document.getElementById("tituput").value;
     document.getElementById("tituput").value = "";
-    /*1*/ nome = document.getElementById("nomeput").value;
+    var nome = document.getElementById("nomeput").value;
     document.getElementById("nomeput").value = "";
-    /*2*/ data = document.getElementById("dataput").value;
+    var data = document.getElementById("dataput").value;
     document.getElementById("dataput").value = "";
-    /*3*/ descri = document.getElementById("descriput").value;
+    var descri = document.getElementById("descriput").value;
     document.getElementById("descriput").value = "";
-    //
-    //arrumar data para padrao br
 
-    datanova = data.split("-").reverse().join("/");
-    // jogar tudo num array
-    const artigo = [titulo, nome, datanova, descri];
+    // Arrumar data para padrão brasileiro
+    var datanova = data.split("-").reverse().join("/");
+
+    // Adicionar os dados em um array
+    var artigo = [titulo, nome, datanova, descri];
     arrayGeral.push(artigo);
   } else {
-    const artigo = [titulo, nome, datanova, descri];
-    artigo[indice] = artigo;
+    var artigo = arrayGeral[indice];
+    artigo[0] = document.getElementById("tituput").value;
+    artigo[1] = document.getElementById("nomeput").value;
+    artigo[2] = document.getElementById("dataput").value;
+    artigo[3] = document.getElementById("descriput").value;
 
     indice = -1;
-    
   }
 
   atualizarListaTela();
-
 }
+
 function atualizarListaTela() {
-  let msg = "";
-  for (let i = 0; i < arrayGeral.length; i++) {
-    console.log(i);
-    tarefa = arrayGeral[i];
+  var msg = "";
+  for (var i = 0; i < arrayGeral.length; i++) {
+    var tarefa = arrayGeral[i];
     msg += `<div id="comentario">
     <p id="titulo">Titulo: ${tarefa[0]} </p>  
     <p id="nome">Nome: ${tarefa[1]} </p> 
     <p id="data">Data de publicação: ${tarefa[2]} </p> 
     <p id="descri">Descrição: ${tarefa[3]} </p> 
     <button type="button" class="btn2" onclick="editar(${i})">Editar</button>
-    <button type="button" class="btn2"onclick="apagar(${i})">Apagar</button>
+    <button type="button" class="btn2" onclick="apagar(${i})">Apagar</button>
     </div>`;
   }
 
@@ -56,24 +54,16 @@ function atualizarListaTela() {
 }
 
 function apagar(i) {
-  msg = arrayGeral.splice(i, 1);
+  arrayGeral.splice(i, 1);
   atualizarListaTela();
 }
-function editar() {
 
+function editar(i) {
+  var tarefa = arrayGeral[i];
   document.getElementById("tituput").value = tarefa[0];
   document.getElementById("nomeput").value = tarefa[1];
   document.getElementById("dataput").value = tarefa[2];
   document.getElementById("descriput").value = tarefa[3];
 
-    /*0*/ titulo = document.getElementById("tituput").value;
-    /*1*/ nome = document.getElementById("nomeput").value;
-    /*2*/ datanova = document.getElementById("dataeput").value;
-    /*3*/ descri = document.getElementById("descriput").value;
-
-    arrayGeral.push(artigo);
-    const artigo = [titulo, nome, datanova, descri];
-
-    atualizarListaTela()
+  indice = i;
 }
-
